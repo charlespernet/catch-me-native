17e4e208b328f87bd0695e4eb87f1698a0d5f79e
Charles Pernet <charlespernet@gmail.com>
2019-03-26 11:04:04 +0100

    WIP Line Icons

--- components/LinesList.js
+++ components/LinesList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 // This tends to be replaced by an API Call
 import {lines} from '../data/lines';
@@ -9,9 +9,13 @@ const styles = StyleSheet.create({
    flex: 1,
    paddingTop: 22
   },
-  item: {
-    padding: 20,
+  itemText: {
     fontSize: 18,
+  },
+  itemIcon: {
+    backgroundColor: 'red',
+    padding: 15,
+    borderRadius: 50,
   },
   separator: {
     height: 1,
@@ -26,17 +30,24 @@ class LinesList extends Component {
   state = lines
 
   _onPress = (item) => {
+    console.log('pressed')
     this.props.navigation.navigate('LineShow', {lineId: item.name})
   }
 
   _renderLine = ({ item }) => {
     return (
-      <Text 
-        style={styles.item} 
-        onPress={()=> this._onPress(item)} 
-      >
-        {item.name}
-      </Text>
+      <TouchableOpacity onPress={()=> this._onPress(item)} activeOpacity={0.8} style={{flex: 1, flexDirection: 'row', padding: 20}} >
+        <View style={{flex: 1}} >
+          <View style={styles.itemIcon}>
+            <Text style={{color: 'white'}}>8</Text>
+          </View>
+        </View>
+        <View style={{flex: 6}}>
+          <Text style={styles.itemText} >
+            {item.name}
+          </Text>
+        </View>
+      </TouchableOpacity>
     )
   }
 
